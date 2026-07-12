70ef915b6b8b4f5b25e25809de0213c791674a2d
SiddanthKarthik <siddanthkarthik@gmail.com>
2026-07-12 16:49:24 -0400

    Updates to schema and created initial seed data

--- database/schema.sql
+++ database/schema.sql
@@ -4,13 +4,21 @@ CREATE TABLE Institution (
     website TEXT,
     city TEXT,
     state TEXT,
-    country TEXT
+    country TEXT,
+    source TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Department (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     institution_id INTEGER NOT NULL,
+    source TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE(name, institution_id),
 
     FOREIGN KEY (institution_id)
         REFERENCES Institution(id)
@@ -22,10 +30,9 @@ CREATE TABLE Professor (
     email TEXT,
     ORCID TEXT UNIQUE,
     website TEXT,
-    department_id INTEGER NOT NULL,
-
-    FOREIGN KEY (department_id)
-        REFERENCES Department(id)
+    source TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Lab (
@@ -37,34 +44,49 @@ CREATE TABLE Lab (
     state TEXT,
     country TEXT,
     department_id INTEGER NOT NULL,
-    pi_professor_id INTEGER NOT NULL,
+    pi_professor_id INTEGER,
+    source TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
     FOREIGN KEY (department_id)
         REFERENCES Department(id),
+
     FOREIGN KEY (pi_professor_id)
         REFERENCES Professor(id)
 );
 
-CREATE TABLE Publication(
+CREATE TABLE Publication (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     abstract TEXT,
     publication_date DATE,
     journal TEXT,
     DOI TEXT UNIQUE,
-    url TEXT
+    url TEXT,
+    source TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE ResearchTopic (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE
+    name TEXT NOT NULL UNIQUE,
+    source TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE ProfessorPublication (
     professor_id INTEGER NOT NULL,
     publication_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
     PRIMARY KEY (professor_id, publication_id),
+
     FOREIGN KEY (professor_id)
         REFERENCES Professor(id),
+
     FOREIGN KEY (publication_id)
         REFERENCES Publication(id)
 );
@@ -72,9 +94,13 @@ CREATE TABLE ProfessorPublication (
 CREATE TABLE PublicationTopic (
     publication_id INTEGER NOT NULL,
     topic_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
     PRIMARY KEY (publication_id, topic_id),
+
     FOREIGN KEY (publication_id)
         REFERENCES Publication(id),
+
     FOREIGN KEY (topic_id)
         REFERENCES ResearchTopic(id)
 );
@@ -82,9 +108,41 @@ CREATE TABLE PublicationTopic (
 CREATE TABLE LabResearchTopic (
     lab_id INTEGER NOT NULL,
     topic_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
     PRIMARY KEY (lab_id, topic_id),
+
     FOREIGN KEY (lab_id)
         REFERENCES Lab(id),
+
     FOREIGN KEY (topic_id)
         REFERENCES ResearchTopic(id)
+);
+
+CREATE TABLE ProfessorDepartment (
+    professor_id INTEGER NOT NULL,
+    department_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (professor_id, department_id),
+
+    FOREIGN KEY (professor_id)
+        REFERENCES Professor(id),
+
+    FOREIGN KEY (department_id)
+        REFERENCES Department(id)
+);
+
+CREATE TABLE ProfessorLab (
+    professor_id INTEGER NOT NULL,
+    lab_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (professor_id, lab_id),
+
+    FOREIGN KEY (professor_id)
+        REFERENCES Professor(id),
+
+    FOREIGN KEY (lab_id)
+        REFERENCES Lab(id)
 );
